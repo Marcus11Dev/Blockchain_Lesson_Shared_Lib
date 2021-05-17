7e40d739e8e08197c9792c116e7a17bd2ddd0d19
Marcus11Dev <63058018+Marcus11Dev@users.noreply.github.com>
2021-05-17 18:52:51 +0200

    Changed functions to private

--- TeamCloud_Modul/TeamCloud_Modul/Node.py
+++ TeamCloud_Modul/TeamCloud_Modul/Node.py
@@ -58,7 +58,7 @@ class Node:
         self.json_parser = JSON_Parser()
 
         if name == "Cloud":
-            if not self.read_backup():
+            if not self._read_backup():
                 self.blockchain.create_genesis_block()
                 self.init_coins()
   
@@ -73,7 +73,7 @@ class Node:
     def init_coins(self):
         self.blockchain.add_new_transaction(Transaction('bank',self.name, 'InitCoin',1,10000,'0'))
         self.blockchain.mine()
-        self.write_backup()
+        self._write_backup()
     
     def update_balance(self):
         self.balance = 0
@@ -159,49 +159,7 @@ class Node:
     def transaction(self, source, destination, product, quantity, amount, signature):
             self.blockchain.add_new_transaction(Transaction(source, destination, product, quantity, amount, signature))
             self.blockchain.mine()
-            self.write_backup()
-
-    def transaction_to(self,to,product, quantity,amount,signature):
-        self.blockchain.add_new_transaction(Transaction(self.name,to,product, quantity, amount,signature))
-        self.blockchain.mine()
-        self.write_backup()
-    
-    def establish_connection(self):
-        return
-        
-    def known_nodes(self):
-        return
-    
-    def validate_chain(self,chain):
-        status = True
-        return status
-
-    def create_dump_from_chain(self):
-        chain_data = []
-        for block in self.blockchain.chain:
-            chain_data.append(block.__dict__)
-        return json.dumps({"length": len(chain_data),
-                            "chain": chain_data})
-    
-    def create_chain_from_dump(self, chain_dump):
-        generated_blockchain = Blockchain()
-        generated_blockchain.create_genesis_block()
-        for idx, block_data in enumerate(chain_dump):
-            if idx == 0:
-                continue  # skip genesis block
-            block = Block(block_data["index"],
-                        block_data["transactions"],
-                        block_data["timestamp"],
-                        block_data["previous_hash"],
-                        block_data["nonce"])
-            proof = block_data['hash']
-            added = generated_blockchain.add_block(block, proof)
-            if not added:
-                raise Exception("The chain dump is tampered!!")
-        return generated_blockchain.chain
-
-    def update_chain(self,chain):
-        self.blockchain.chain = chain
+            self._write_backup()
 
     # Blockchain synchronisation part START
 
@@ -216,7 +174,7 @@ class Node:
         
         return start_hash, stop_hash
 
-    def get_payload_for_headers_msg(self,start_hash,stop_hash):
+    def _get_payload_for_headers_msg(self,start_hash,stop_hash):
         """
             Returns a list of hashes of Blocks from the blockchain.
             The lists starts with the block after the one with the "start_hash".
@@ -245,7 +203,6 @@ class Node:
         """
         # create 2D Matrix where the Value stands for the first Index where the two lists differ
 
-        #print("self.headers = ", self.headers)
         # if there are no new headers
         if self.headers == []:
             # Either it was forgotten to make a get_headers request or the request brought no new headers
@@ -271,11 +228,8 @@ class Node:
                         differ_index[index_list_2][index_list_1] = index_item_in_list + 1
                         break
 
-        #print(differ_index)
-
         # find the best headers list. There are two criterias:
-        
-       
+               
         # 1. the list has the most in common with other lists (=> sum of all diff_indexes is maximum)
         summed_values = [sum(i) for i in differ_index]
         #print("Summe übereinstimmungen =", summed_values)
@@ -287,15 +241,12 @@ class Node:
             max_indexes.append(summed_values.index(abs_max))
             if max(summed_values) == 0:
                 return []
-                #raise ValueError("The provided headers are all completely diffrent!")
             summed_values[max_indexes[-1]] = 0
 
         len_of_max_index_lists = [len(self.headers[i]) for i in max_indexes]
-        #print("Laenge der besten Listen: ",len_of_max_index_lists)
         
         # 2. it is the longest list of all lists that satisfy 1.
         best_headers_list_index = max_indexes[len_of_max_index_lists.index(max(len_of_max_index_lists))]
-        #print("Best and longest list is: ", best_headers_list_index) 
               
         payload = []
         for hash_ in self.headers[best_headers_list_index]:
@@ -307,7 +258,7 @@ class Node:
         return payload #, best_headers_list_index
 
 
-    def get_payload_for_block_msg(self,block_hash):
+    def _get_payload_for_block_msg(self,block_hash):
         """
             Returns the block-object corresponding to the provided block_hash.
         """
@@ -318,21 +269,8 @@ class Node:
                 payload = block
                 exit
         return payload
-
-    def get_message_created(self,receiver,payload, parser_type = 'type_default', message_type = None):
-        """
-            Creates a message object with the provided content.
-            Adds automatically a md5 hash as checksum.
-        """
-        checksum = md5(str(payload).encode()).hexdigest()
-        message = Message(sender=self.name,receiver=receiver, 
-                        parser_type=parser_type,
-                        message_type=message_type, 
-                        payload=payload,
-                        checksum=checksum)
-        return message
     
-    def handle_get_headers_msg(self,get_headers_msg):
+    def _handle_get_headers_msg(self,get_headers_msg):
         """
             Returns a message object containing all hashes of the blocks 
             between the start and stop hash.
@@ -342,7 +280,7 @@ class Node:
         start_hash = get_headers_msg.payload[0]
         stop_hash = get_headers_msg.payload[1]
         # create a headers_msg as a response
-        payload = self.get_payload_for_headers_msg(start_hash,stop_hash)
+        payload = self._get_payload_for_headers_msg(start_hash,stop_hash)
 
         # Built Message
         message = Message(sender=self.name,
@@ -354,7 +292,7 @@ class Node:
 
         return message
 
-    def handle_headers_msg(self,get_headers_msg):
+    def _handle_headers_msg(self,get_headers_msg):
         """
             Safes the headers to self.headers list
         """
@@ -365,7 +303,7 @@ class Node:
         return None
 
 
-    def handle_get_blocks_msg(self, get_headers_msg):
+    def _handle_get_blocks_msg(self, get_headers_msg):
         """
             Creates and returns a block_msg as a response.
             This Message contains a list of all the blocks corresponding to the hash list provided.
@@ -373,8 +311,7 @@ class Node:
         # Workaround to send all the requested blocks. Should be in seperate messages but is now done by one message with a list
         payload = []
         for hash_chain in [x[1] for x in get_headers_msg.payload]:
-            payload.append(self.get_payload_for_block_msg(hash_chain))
-        #message = self.get_message_created(receiver=get_headers_msg[0],payload=payload,message_type='block_msg')
+            payload.append(self._get_payload_for_block_msg(hash_chain))
 
         message = Message(sender=self.name,
                     receiver=get_headers_msg.sender,
@@ -385,7 +322,7 @@ class Node:
 
         return message
 
-    def handle_block_msg(self, block_msg):
+    def _handle_block_msg(self, block_msg):
         """
             Retrieves all blocks from the block message and checks if it is a valid blockchain and fits to the current one.
             If everything is alright it  appends the blocks to the blockchain.
@@ -447,31 +384,26 @@ class Node:
         """
         message_checksum = md5(str(incoming_message.payload).encode()).hexdigest()
         return_message = None 
-
-        #if message_checksum == incoming_message.checksum: # Check if Message was received correctly
-        if True: # Check if Message was received correctly
-            if incoming_message.message_type=='get_headers_msg':
-                return_message = self.handle_get_headers_msg(incoming_message)
-            
-            elif incoming_message.message_type=='headers_msg':
-                self.handle_headers_msg(incoming_message)
-            
-            elif incoming_message.message_type=='get_blocks_msg':
-                return_message = self.handle_get_blocks_msg(incoming_message)
-            
-            elif incoming_message.message_type=='block_msg':
-                self.handle_block_msg(incoming_message)
-                return_message = None            
-            
-            else:
-                print("Incoming message has unknown message Type!") # Undefined Message Type received
+        if incoming_message.message_type=='get_headers_msg':
+            return_message = self._handle_get_headers_msg(incoming_message)
+        
+        elif incoming_message.message_type=='headers_msg':
+            self._handle_headers_msg(incoming_message)
+        
+        elif incoming_message.message_type=='get_blocks_msg':
+            return_message = self._handle_get_blocks_msg(incoming_message)
+        
+        elif incoming_message.message_type=='block_msg':
+            self._handle_block_msg(incoming_message)
+            return_message = None            
+        
         else:
-            print("Checksum of incoming message did not match!") # False Checksum, message was not received correctly
+            print("Incoming message has unknown message Type!") # Undefined Message Type received
         return return_message
     # Blockchain synchronisation part END
 
-    def get_chain(self):
-        return self.blockchain.chain
+    # def get_chain(self):
+    #     return self.blockchain.chain
 
     def create_signature(self, payload):
         byte_payload = str(payload).encode('utf-8')
@@ -523,7 +455,8 @@ class Node:
         else:
             return True
 
-    def write_backup(self):
+
+    def _write_backup(self):
         if os.path.exists(self.backup_path) and os.path.getsize(self.backup_path) > 0:
             os.remove(self.backup_path)
 
@@ -536,7 +469,7 @@ class Node:
         with open(self.backup_path, "w") as f:
             json.dump(json_obj,f)
 
-    def read_backup(self):
+    def _read_backup(self):
         # Check Text-File already Exists and isn't empty
         if os.path.exists(self.backup_path) and os.path.getsize(self.backup_path) > 0:
             with open(self.backup_path, "r") as f:
